32011f51596fa76633a6d0e5dbfd2c91441388ab
BarreiraTom <tomas.barreira14@outlook.pt>
2020-02-02 18:40:58 +0000

    update index.js and added views folder

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express= require("express");
 
 const app= express();
+app.use("view engine", "ejs");
 
-app.listen(3000, function() {
+app.get("/", (req,res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
     console.log('Hello');
 });
