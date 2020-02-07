c54986773cadeab28f41a9622d02e67db0d9d7f4
BarreiraTom <tomas.barreira14@outlook.pt>
2020-02-07 00:47:37 +0000

    Updated home.ejs and index.js, and added new test JSON file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express= require("express");
+const fs = require('fs');
 
 const app= express();
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
+
+let jsonHome = fs.readFileSync('resources/json/home.json');
 
 app.get("/", (req,res) => {
-    res.render('home');
+    res.render('home', {
+        pageTitle: 'Home',
+        jsonData: JSON.parse(jsonHome)
+    });
 });
 
 app.listen(3000, () => {
