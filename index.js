557974a41e6a24d8c2fc34f5d1fa27185c49fead
Tomás Barreira <tomas.barreira@cldware.com>
2020-03-24 20:01:00 +0000

    Update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const path = require('path');
 const express= require("express");
 const fs = require('fs');
 
 const app= express();
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'resources')));
 
 let jsonHome = fs.readFileSync('resources/json/home.json');
 
 app.get("/", (req,res) => {
-    res.render('home', {
+    res.render('app-interface/home', {
         pageTitle: 'Home',
         jsonData: JSON.parse(jsonHome)
     });
